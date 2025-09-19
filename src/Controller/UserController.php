32702175ecd7a310f329ea1cad23e5c30e350afa
Solene Baudic <solene.baudic@gmail.com>
2025-09-19 11:48:52 +0200

    Feat - create api function to add, modify or delete an user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,18 +2,126 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 final class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
+  #[Route('/api/users', name: 'user', methods: ['GET'])]
+  public function getUsers(UserRepository $userRepository, SerializerInterface $serializer): JsonResponse
+  {
+    $users = $userRepository->findAll();
+    $jsonUsers = $serializer->serialize($users, 'json');
+    return new JsonResponse($jsonUsers, Response::HTTP_OK, [], true);
+  }
+
+  #[Route('/api/user/{id}', name: 'delete-user', methods: ['DELETE'])]
+  public function deleteUser(int $id, UserRepository $userRepository, EntityManagerInterface $entityManager): JsonResponse
+  {
+    $user = $userRepository->find($id);
+
+    if (!$user) {
+      return new JsonResponse(
+        ['error' => 'User not found'],
+        Response::HTTP_NOT_FOUND
+      );
     }
+
+    $entityManager->remove($user);
+    $entityManager->flush();
+
+    return new JsonResponse(
+      ['message' => "User {$id} deleted successfully"],
+      Response::HTTP_OK,
+      [],
+    );
+  }
+
+  #[Route('/api/user/{id}', name: 'modify-user', methods: ['PUT'])]
+  public function modifyUser(
+    int $id,
+    UserRepository $userRepository,
+    EntityManagerInterface $entityManager,
+    Request $request,
+    UserPasswordHasherInterface $passwordHasher
+  ): JsonResponse {
+    $user = $userRepository->find($id);
+
+    if (!$user) {
+      return new JsonResponse(
+        ['error' => 'User not found'],
+        Response::HTTP_NOT_FOUND
+      );
+    }
+
+    $data = json_decode($request->getContent(), true);
+
+    if (isset($data['login'])) {
+      $user->setLogin($data['login']);
+    }
+
+    if (isset($data['postcode'])) {
+      $user->setPostcode($data['postcode']);
+    }
+
+    if (isset($data['password'])) {
+      $hashedPassword = $passwordHasher->hashPassword($user, $data['password']);
+      $user->setPassword($hashedPassword);
+    }
+
+    $entityManager->flush();
+
+    return new JsonResponse(
+      ['message' => "User {$id} updated successfully"],
+      Response::HTTP_OK,
+      [],
+    );
+  }
+
+  #[Route('/api/user/new', name: 'create-user', methods: ['POST'])]
+  public function createUser(
+    EntityManagerInterface $entityManager,
+    Request $request,
+    UserPasswordHasherInterface $passwordHasher
+  ): JsonResponse {
+
+    $data = json_decode($request->getContent(), true);
+    $user = new User();
+    if (isset($data['login'])) {
+      $user->setLogin($data['login']);
+    }
+
+    if (isset($data['postcode'])) {
+      $user->setPostcode($data['postcode']);
+    }
+
+    if (isset($data['password'])) {
+      $hashedPassword = $passwordHasher->hashPassword($user, $data['password']);
+      $user->setPassword($hashedPassword);
+    }
+
+    $entityManager->persist($user);
+    $entityManager->flush();
+
+    return new JsonResponse(
+      [
+        'message' => "User created successfully",
+        'user' => [
+          'id' => $user->getId(),
+          'login' => $user->getLogin(),
+          'postcode' => $user->getPostcode()
+        ]
+      ],
+      Response::HTTP_CREATED,
+      [],
+    );
+  }
 }
